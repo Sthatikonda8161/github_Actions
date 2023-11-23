e28a59e1afbbae3b32622382626315a14c8ccde9
Saikiran Thatikonda <136583514+Sthatikonda8161@users.noreply.github.com>
2023-11-23 18:37:42 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,18 +1,53 @@
-from flask import Flask
 import pytest
+import csv
+from flask import Flask
 
-@pytest.fixture
-def client():
-    app = Flask(__name__)
-    app.config['TESTING'] = True
+app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def helloWorld():
+    return 'Hello World'
+
+@app.route('/aboutUs')
+def aboutUs():
+    return 'this is about us page'
+
+@app.route('/contactus', methods=['POST'])
+def contactUs():
+    return 'this is contact us page'
+
+if __name__ == '__main__':
+    app.run(port=3000, debug=True)
+
+def test_helloWorld():
     with app.test_client() as client:
-        yield client
+        response = client.get('/')
+        assert response.status_code == 200
+        assert response.get_data(as_text=True) == 'Hello World'
 
-def test_hello(client):
-    response = client.get('/')
-    assert b'Hello' in response.data
 
-def test_aboutus(client):
-    response = client.get('/aboutus')
-    assert b'About Us' in response.data
+    with open('results.csv', 'a') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['test_helloWorld', 'passed'])
+
+
+def test_aboutUs():
+    with app.test_client() as client:
+        response = client.get('/aboutUs')
+        assert response.status_code == 200
+        assert response.get_data(as_text=True) == 'this is about us page'
+
+    with open('results.csv', 'a') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['test_aboutUs', 'passed'])
+
+
+def test_contactUs():
+    with app.test_client() as client:
+        response = client.post('/contactus')
+        assert response.status_code == 200
+        assert response.get_data(as_text=True) == 'this is contact us page'
+
+    with open('results.csv', 'a') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['test_contactUs', 'passed'])
